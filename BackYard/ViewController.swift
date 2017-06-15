1139e11aab294a07b520240759b772c96f8546d9
icbrahimc <icbrahimc@Ibrahims-MacBook-Pro.local>
2017-06-15 00:09:22 -0700

    Added view setup method

--- BackYard/ViewController.swift
+++ BackYard/ViewController.swift
@@ -15,20 +15,22 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let newView = UIView(frame: UIScreen.main.bounds)
-        newView.backgroundColor = UIColor.red
-        self.view.addSubview(newView)
-        
-        let loginButton: FBSDKLoginButton = FBSDKLoginButton()
-        loginButton.center = self.view.center
-        
-        view.addSubview(loginButton)
+        setup()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func setup() -> Void {
+        // Setup the login button.
+        let loginButton: FBSDKLoginButton = FBSDKLoginButton()
+        loginButton.center = self.view.center
+        view.addSubview(loginButton)
+        
+        self.view.backgroundColor = .white
     }
 
 
