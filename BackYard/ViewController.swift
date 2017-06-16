b8fbf44ff233d1b1af57ed23c6a07444f654ca44
icbrahimc <icbrahimc@Ibrahims-MacBook-Pro.local>
2017-06-16 01:29:14 -0700

    Added the full facebook view to the view

--- BackYard/ViewController.swift
+++ BackYard/ViewController.swift
@@ -11,7 +11,7 @@ import FBSDKCoreKit
 import FBSDKLoginKit
 import UIKit
 
-class ViewController: UIViewController, FBSDKLoginButtonDelegate {
+class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,68 +26,55 @@ class ViewController: UIViewController, FBSDKLoginButtonDelegate {
     }
     
     func setup() -> Void {
+        // Offsets for the screen.
+//        let offsetX: CGFloat = UIScreen.main.bounds.maxX
+        let offsetY: CGFloat = UIScreen.main.bounds.maxY
+        
         // Setup the login button.
-        let loginButtonFrame = CGRect(x: 0, y: 0, width: view.frame.width - 32, height: 50)
-        let fbColor = UIColor.init(red: 59, green: 89, blue: 152, alpha: 1.0)
+        let loginButtonFrame = CGRect(x: 0, y: offsetY * 0.8
+            , width: view.frame.width - 32, height: 50)
         
-        
+        // Custom Facebook login.
         let fbLogin = UIButton(type: .system)
         fbLogin.frame = loginButtonFrame
         fbLogin.setTitle("Log In With Facebook", for: .normal)
-        fbLogin.setTitleColor(fbColor, for: .normal)
-        fbLogin.backgroundColor = .black
+        fbLogin.setTitleColor(.white, for: .normal)
+        fbLogin.backgroundColor = .blue
         fbLogin.titleLabel?.font = UIFont.boldSystemFont(ofSize: 18)
+        fbLogin.layer.cornerRadius = 25.0
+        fbLogin.layer.masksToBounds = true
+        fbLogin.center.x = self.view.center.x
+        
         fbLogin.addTarget(self, action: #selector(handleFaceBookLogin), for: .touchUpInside)
-        fbLogin.center = self.view.center
         view.addSubview(fbLogin)
-        
-        let loginButton: FBSDKLoginButton = FBSDKLoginButton(frame: loginButtonFrame)
-        loginButton.center = self.view.center
-        loginButton.layer.cornerRadius = 25.0
-        loginButton.layer.masksToBounds = true
-        loginButton.delegate = self
-        
-//        view.addSubview(loginButton)
         
         self.view.backgroundColor = .white
     }
-    
-    func handleFaceBookLogin() {
-        print("Hey")
-    }
 
-    func loginButton(_ loginButton: FBSDKLoginButton!, didCompleteWith result: FBSDKLoginManagerLoginResult!, error: Error!) {
-        if let error = error {
-            print(error.localizedDescription)
-            return
-        }
-        
-        // Get the access token and authenticate.
-        let accessToken = FBSDKAccessToken.current()
-        guard let accessTokenString = accessToken?.tokenString else {
-            return
-        }
-        
-        let credential = FacebookAuthProvider.credential(withAccessToken: accessTokenString)
-        Auth.auth().signIn(with: credential) { (user, error) in
-            // ...
-            if let error = error {
+    /** Facebook Auth */
+    func handleFaceBookLogin() {
+        FBSDKLoginManager().logIn(withReadPermissions: ["email", "public_profile"], from: self, handler: {
+            (result, err) in
+            if let error = err {
                 print(error.localizedDescription)
                 return
             }
-        }
+            
+            // Get the access token and authenticate.
+            let accessToken = FBSDKAccessToken.current()
+            guard let accessTokenString = accessToken?.tokenString else {
+                return
+            }
+            
+            let credential = FacebookAuthProvider.credential(withAccessToken: accessTokenString)
+            Auth.auth().signIn(with: credential) { (user, error) in
+                // ...
+                if let error = error {
+                    print(error.localizedDescription)
+                    return
+                }
+            }
+        })
     }
-    
-    func loginButtonDidLogOut(_ loginButton: FBSDKLoginButton!) {
-        let firebaseAuth = Auth.auth()
-        do {
-            try firebaseAuth.signOut()
-        } catch let signOutError as NSError {
-            print ("Error signing out: %@", signOutError)
-        }
-        
-    }
-    
-
 }
 
