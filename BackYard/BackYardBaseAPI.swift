1a008e534f184152d4cc9d0ec73cc74bea631565
Omar Baradei <omarbaradei21@gmail.com>
2017-07-15 17:26:16 -0400

    Added getUserData api

--- BackYard/BackYardBaseAPI.swift
+++ BackYard/BackYardBaseAPI.swift
@@ -11,6 +11,23 @@ import Firebase
 import FirebaseDatabase
 
 class BackYardBaseAPI: NSObject {
-    static let sharedInstance = BackYardBaseAPI()
-    static var rootDB: DatabaseReference! = Database.database().reference()
+    //static let backyardAPI = BackYardBaseAPI()
+    private let rootDB: DatabaseReference! = Database.database().reference()
+    
+    
+    /**
+     Given a user's UUID, fetch the data associated with that user.
+    */
+    func getUserData(uuid: String, callback: @escaping (String, String, String, String, String) -> ()) {
+        rootDB.child("userDB").child(uuid).observe(.value, with: { (snapshot) in
+            if let dictionary = snapshot.value as? [String : Any] {
+                let firstName = dictionary["firstName"] as? String ?? "",
+                    lastName = dictionary["lastName"] as? String ?? "",
+                    imageURL = dictionary["imageURL"] as? String ?? "",
+                    email = dictionary["email"] as? String ?? "",
+                    username = dictionary["username"] as? String ?? ""
+                callback((firstName, lastName, imageURL, email, username))
+            }
+        })
+    }
 }
